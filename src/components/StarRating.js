dd88f07f0b3ae61f138e21e03683f373ab61998b
kacarervin <kacarervin@gmail.com>
2022-11-03 09:43:51 +0100

    add images and style for images

---
+++ src/components/StarRating.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const EMPTY_STAR =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/1088px-Five-pointed_star.svg.png";
+const FULL_STAR =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Gold_Star.svg/1200px-Gold_Star.svg.png";
+
+const StarRating = () => {
+  const starsIds = [1, 2, 3, 4, 5];
+  const [hovered, setHovered] = useState(0);
+  const [clicked, setClicked] = useState(0);
+
+  const getImg = (id) => {
+    return hovered >= id || clicked >= id ? FULL_STAR : EMPTY_STAR;
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", padding: 20 }}>
+      {starsIds.map((id) => (
+        <img src={getImg(id)} width={60} height={60} />
+      ))}
+    </div>
+  );
+};
+
+export default StarRating;
